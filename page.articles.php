f31a27e8cedbb183a73f81e75e1a46c1ed9d2beb
HyunJun Kim <me@hyunjun.org>
2016-12-22 10:47:14 +0900

    Update articles page

--- page.articles.php
+++ page.articles.php
@@ -1,4 +1,40 @@
 <?php
+include "module.utils.php";
+
+$pageTitle = "나의 오늘";
+$pageAuthor = "김현준";
+
+include "frame.header.php";
+?>
+
+<div class="container" style="margin-top: 10%">
+
+<input class="u-full-width" type="email" placeholder="검색어 입력" id="exampleEmailInput">
+
+<hr/>
+
+<h4>2016. 12. 22</h4>
 
 
+<textarea class="u-full-width" placeholder="삶의 조각"id="exampleMessage"></textarea>
+<a class="button u-full-width" style="margin-bottom: 5em" href="#">기록하기</a>
+
+
+<h4>2016. 12. 21</h4>
+<p> 아침 늦게 기상. 삼성동 COEX에서 재훈이형을 만나 중식을 든든히 먹은 후, 인근 ASEM 타워에서 <code>***</code>님과 설리번 프로젝트에 관한 미팅을 가졌다.
+프로젝트의 향후 진행에 대한 의견과 후원 가능성을 말씀해 주셨으나, 이 기회에 대한 준비가 되지 않았다는 사실은 이미 나 스스로가 잘 아는 터. 비오는 삼성동의 우울한 풍광에 젖었다.
+축 젖은 채 송도에 돌아와서 <단상>을 만들다.
+</p>
+
+
+<h4>2016. 12. 20</h4>
+<p> 아침 늦게 기상. 삼성동 COEX에서 재훈이형을 만나 중식을 든든히 먹은 후, 인근 ASEM 타워에서 <code>***</code>님과 설리번 프로젝트에 관한 미팅을 가졌다.
+프로젝트의 향후 진행에 대한 의견과 후원 가능성을 말씀해 주셨으나, 이 기회에 대한 준비가 되지 않았다는 사실은 이미 나 스스로가 잘 아는 터. 비오는 삼성동의 우울한 풍광에 젖었다.
+축 젖은 채 송도에 돌아와서 <단상>을 만들다.
+</p>
+
+</div>
+
+<?php
+include "frame.footer.php";
 ?>
